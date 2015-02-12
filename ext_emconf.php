64b0cf481de30b1c5ffbb9bde4ea235809b9932e
Oliver Hader <oliver@typo3.org>
2015-02-12 19:17:31 +0100

    [TASK] Add extension dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearcacheonload' => 1,
-	'author' => 'Introduction Package Team',
+	'author' => 'Government Package Team',
 	'author_email' => '',
 	'author_company' => '',
 	'constraints' => 
 	array(
 		'depends' => array(
 			'typo3' => '6.2.0-6.2.99',
-			'realurl' => '1.12.8-1.12.99',
+			'realurl' => '1.12.8-0.0.0',
+			'a21glossary' => '6.2.0-0.0.0',
+			'contrast' => '0.1.4-0.0.0',
+			'menu_balancer' => '0.9.1-0.0.0',
+			'news' => '3.0.1-0.0.0',
+			't3colorbox' => '3.0.0-0.0.0',
+			'wt_spamshield' => '1.3.0-0.0.0',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
